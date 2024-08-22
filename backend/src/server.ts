5bcf6e5b0274b410a13dff3c81523264205396df
Aries Dmello <dmelloaries@gmail.com>
2024-08-22 22:59:43 +0530

    migrated to typescript

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Server is live');
 });
 
